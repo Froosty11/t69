c3c44fc7b0e7bfaff04f8b143231cfeac7f939bf
Edvin F <edvin.frosterud@gmail.com>
2022-10-17 15:32:38 +0200

    initial commit

---
+++ src/Helper.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Helper {
+    /**
+     * Returns full dictonary of swedish words. Root contains 27 letters that are the start for each word
+     * @return root of the tree
+     */
+    public static Node generateDictonary(){
+        Node root = new Node();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/kelly.txt"))){
+            String line = "";
+            while((line = br.readLine()) != null){
+                Node current = root;
+                System.out.println(line);
+                for(char c : line.toCharArray()){
+                    int i = charTranslator(c);
+                    if(current.next[i] == null)
+                        current.next[i] = new Node();
+                    current = current.next[i];
+                }
+                current.word = true;
+                System.out.println();
+            }
+        }
+        catch (IOException fe) {
+            System.out.println("File not found");
+        }
+        return root;
+    }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    static char intTranslator(int i){
+        char c = '0';
+        c = (char)(i+97);
+        if(i >= 16) c++;
+        if(i >= 19) c++;
+        if(i == 24) c = 'ä';
+        if(i == 25) c = 'å';
+        if(i == 26) c = 'ö';
+        return c;
+    }
+    public static int charTranslator(char c){
+        int character = (int)c;
+        if(character == 113) character++;
+        if(character == 119) character++;
+        System.out.println((char)character);
+        character -= 97;
+        if(character == 131) character = 24;
+        if(character == 132) character = 25;
+        if(character == 149) character = 26;
+        return character;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,44 +5,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Node root = new Node();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/kelly.txt"))){
-            String line = "";
-            while((line = br.readLine()) != null){
-                Node current = root;
-                System.out.println(line);
-                for(char c : line.toCharArray()){
-                    int i = charTranslator(c);
-                    if(current.next[i] == null)
-                    current.next[i] = new Node();
-                    current = current.next[i];
-                }
-                current.word = true;
-                System.out.println();
+        //Node root = Helper.generateDictonary();
+        System.out.println(
+                Helper.intTranslator(18) + "" +
+                        Helper.intTranslator(19) +
+                        Helper.intTranslator(20));
+    }
 
-            }
-        }
-        catch (IOException fe) {
-            System.out.println("File not found");
-        }
-        System.out.println("cock");
-    }
-    /*
-    HELLO THIS IS DOCUMENTATION
-    THIS TRANSLATES A CHAR TO A NUMBER CORRESPONDING
-    IT SKIPS W, Q AND HAS ÄÅÖ
-     */
-    static int charTranslator(char c){
-        int character = (int)c;
-        if(character == 113) character++;
-        if(character == 119) character++;
-        System.out.println((char)character);
-        character -= 97;
-        if(character == 131) character = 24;
-        if(character == 132) character = 25;
-        if(character == 149) character = 26;
-        return character;
-    }
 
     /*
     "5 1 4 666"
